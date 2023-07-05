f6538aeda2c6c3d4bd3bc7a01c43905f1543eda7
Fredrick <mwangikingz94@gmail.com>
2023-07-05 09:33:50 +0300

    added components

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Login</li>
+        <li>Sign Up</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
