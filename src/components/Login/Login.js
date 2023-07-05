e30627bf3424aed20eca7a9d2cd62b62a4d8dd9f
Fredrick <mwangikingz94@gmail.com>
2023-07-05 16:14:37 +0300

    to be continued

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -16,33 +16,75 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
+    <div style={styles.container}>
+      <h2 style={styles.heading}>Login</h2>
       <form>
-        <div>
-          <label htmlFor="email">Email:</label>
+        <div style={styles.formGroup}>
+          <label htmlFor="email" style={styles.label}>Email:</label>
           <input
             type="email"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            style={styles.input}
           />
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
+        <div style={styles.formGroup}>
+          <label htmlFor="password" style={styles.label}>Password:</label>
           <input
             type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            style={styles.input}
           />
         </div>
-        <button type="button" onClick={handleLogin}>
+        <button type="button" onClick={handleLogin} style={styles.button}>
           Login
         </button>
       </form>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    maxWidth: '400px',
+    margin: '0 auto',
+    padding: '20px',
+    backgroundColor: '#f2f2f2',
+    borderRadius: '4px',
+  },
+  heading: {
+    textAlign: 'center',
+    marginBottom: '20px',
+  },
+  formGroup: {
+    marginBottom: '10px',
+  },
+  label: {
+    display: 'block',
+    marginBottom: '5px',
+    fontWeight: 'bold',
+  },
+  input: {
+    width: '100%',
+    padding: '8px',
+    fontSize: '16px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+  },
+  button: {
+    width: '100%',
+    padding: '10px',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    backgroundColor: '#4caf50',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  },
 };
 
 export default Login;
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -19,42 +19,85 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <h2>Sign Up</h2>
+    <div style={styles.container}>
+      <h2 style={styles.heading}>Sign Up</h2>
       <form>
-        <div>
-          <label htmlFor="username">Username:</label>
+        <div style={styles.formGroup}>
+          <label htmlFor="username" style={styles.label}>Username:</label>
           <input
             type="text"
             id="username"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
+            style={styles.input}
           />
         </div>
-        <div>
-          <label htmlFor="email">Email:</label>
+        <div style={styles.formGroup}>
+          <label htmlFor="email" style={styles.label}>Email:</label>
           <input
             type="email"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            style={styles.input}
           />
         </div>
-        <div>
-          <label htmlFor="password">Password:</label>
+        <div style={styles.formGroup}>
+          <label htmlFor="password" style={styles.label}>Password:</label>
           <input
             type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            style={styles.input}
           />
         </div>
-        <button type="button" onClick={handleSignUp}>
+        <button type="button" onClick={handleSignUp} style={styles.button}>
           Sign Up
         </button>
       </form>
     </div>
   );
+};
+
+const styles = {
+  container: {
+    maxWidth: '400px',
+    margin: '0 auto',
+    padding: '20px',
+    backgroundColor: '#f2f2f2',
+    borderRadius: '4px',
+  },
+  heading: {
+    textAlign: 'center',
+    marginBottom: '20px',
+  },
+  formGroup: {
+    marginBottom: '10px',
+  },
+  label: {
+    display: 'block',
+    marginBottom: '5px',
+    fontWeight: 'bold',
+  },
+  input: {
+    width: '100%',
+    padding: '8px',
+    fontSize: '16px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+  },
+  button: {
+    width: '100%',
+    padding: '10px',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    backgroundColor: '#4caf50',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+  },
 };
 
 export default SignUp;
